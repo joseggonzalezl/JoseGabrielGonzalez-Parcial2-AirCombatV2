3d4151122aa46d610f978778dfbf779c67298922
joseggonzalezl <josegonzalez9616@hotmail.com>
2018-04-19 01:56:04 -0500

    Jose Gabriel Gonzalez - Parcial 2 - Version 1

---
+++ JoseGabrielGonzalez-AirCombatV2/Assets/Scripts/Missile.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Destroy (this.gameObject, 5);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		this.transform.Translate (0.0f, 0.0f, 5.0f);
+
+	}
+
+		void OnCollisionEnter(Collision collision) {
+		if (collision.collider.gameObject.tag == "MarsAlien") {
+			GameObject explosion = GameObject.Instantiate (Resources.Load ("Prefabs/MarsExplosion") as GameObject);
+			explosion.transform.position = collision.collider.gameObject.transform.position;
+
+	
+
+			Destroy (collision.collider.gameObject);
+
+		
+
+			Destroy (this.gameObject);
+	
+		}
+
+		if (collision.collider.gameObject.tag == "JupiterAlien") {
+			GameObject explosion = GameObject.Instantiate (Resources.Load ("Prefabs/JupiterExplosion") as GameObject);
+			explosion.transform.position = collision.collider.gameObject.transform.position;
+
+
+			Destroy (collision.collider.gameObject);
+
+			Destroy (this.gameObject);
+
+		}
+
+		if (collision.collider.gameObject.tag == "NeptuneAlien") {
+			GameObject explosion = GameObject.Instantiate (Resources.Load ("Prefabs/NeptuneExplosion") as GameObject);
+			explosion.transform.position = collision.collider.gameObject.transform.position;
+
+
+
+			Destroy (collision.collider.gameObject);
+
+
+
+			Destroy (this.gameObject);
+
+	}
+
+}
+
+}
---
+++ JoseGabrielGonzalez-AirCombatV2/Assets/Scripts/Shoot.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// mover el avion
+
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			this.transform.Translate (0.0f, 2.0f, 0.0f);
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			this.transform.Translate (0.0f, -2.0f, 0.0f);
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			this.transform.Translate (2.0f, 0.0f, 0.0f);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			this.transform.Translate (-2.0f, 0.0f, 0.0f);
+		}
+		// avion avanza hacia adelante
+
+		{
+			transform.Translate(Vector3.forward * Time.deltaTime*20);
+		}
+
+			
+
+		// si el usuario presiona la tecla espacio
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			// Creo un game object a partit de la informacion giardada
+			// en un prefab
+			GameObject missile = GameObject.Instantiate (Resources.Load ("Prefabs/Missile") as GameObject);
+
+			//hago que else misil sea hijo del avion 
+			missile.transform.SetParent(this.transform);
+
+			//Muevo el misil a la posicion 
+			missile.transform.localPosition= new Vector3 (3.73f,-2.23f,3.8f);
+
+		// saco al misil del avion 
+			missile.transform.SetParent(null);
+
+		
+
+
+
+
+		}
+
+	}
+}
